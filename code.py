dcb9ecbeedd210f77af60c21b57af87822b2099f
Namratha <namrathapmd@gmail.com>
2023-08-15 13:39:14 -0500

    function to display the board

---
+++ code.py
@@ -0,0 +1,9 @@
+# Display Board: to display the board
+
+def display_board(board):
+    print(f'{board[1]}  |  {board[2]}  |  {board[3]}')
+    print('______________\n')
+    print(f'{board[4]}  |  {board[5]}  |  {board[6]}')
+    print('______________\n')
+    print(f'{board[7]}  |  {board[8]}  |  {board[9]}')
+    print('______________\n')
